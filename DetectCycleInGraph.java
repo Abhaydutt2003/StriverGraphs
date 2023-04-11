e24536b4ce55dbeeaa34c42a53b80174f2bb5695
Abhaydutt2003 <dutt.abhay@gmail.com>
2023-04-11 15:12:29 +0530

    Update DetectCycleInGraph.java

--- DetectCycleInGraph.java
+++ DetectCycleInGraph.java
@@ -1,20 +1,21 @@
-package StiverGraphs;
+
+    package StiverGraphs;
 
 import java.util.*;
 
 public class DetectCycleInGraph {
 
     public static void main(String args[]) {
-         int v = 5;
-         visited = new boolean[v];
+        int v = 5;
+        visited = new boolean[v];
         // for(int i = 0;i<visited.length;i++){
-        //     if(visited[i] == false){
-        //         bfs();
-        //     }
+        // if(visited[i] == false){
+        // bfs();
+        // }
         // }
     }
 
-    //wrong
+    // wrong
     public static boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
         Queue<Integer> q = new LinkedList<Integer>();
         q.add(0);
@@ -38,21 +39,22 @@ public class DetectCycleInGraph {
     }
 
     static boolean visited[];
-    //right approach , basically the same intution as the above approach..
-    //will need to call this method in all the componenets of the graph..
-    public static boolean bfs(ArrayList<ArrayList<Integer>> adj, int src){
+
+    // right approach , basically the same intution as the above approach..
+    // will need to call this method in all the componenets of the graph..
+    public static boolean bfs(ArrayList<ArrayList<Integer>> adj, int src) {
         Queue<myPair> q = new LinkedList<myPair>();
         q.add(new myPair(-1, src));
         visited[src] = true;
-        while(q.isEmpty() == false){
+        while (q.isEmpty() == false) {
             int currentNode = q.peek().y;
             int currentParent = q.poll().x;
-            ArrayList<Integer> currentList  = adj.get(currentNode);
-            for(int a : currentList){
-                if(visited[a] == false){
+            ArrayList<Integer> currentList = adj.get(currentNode);
+            for (int a : currentList) {
+                if (visited[a] == false) {
                     q.add(new myPair(currentNode, a));
                     visited[a] = true;
-                }else if(a != currentParent){
+                } else if (a != currentParent) {
                     return true;
                 }
             }
@@ -60,6 +62,20 @@ public class DetectCycleInGraph {
         return false;
     }
 
-
+    // using dfs
+    public static boolean dfs(ArrayList<ArrayList<Integer>> adj, int src, int parent){
+        if(visited[src] == true){
+            return true;
+        }
+        visited[src] = true;
+        ArrayList<Integer> current = adj.get(src);
+        for(int i : current){
+            if(i != parent){
+                boolean smallAns = dfs(adj,i,src);
+                if(smallAns){return true;}
+            }
+        }
+        return false;
+    }
 
 }
